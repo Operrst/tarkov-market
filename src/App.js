c6df2702bad04ed76b8bfc746b132b869a8826bf
Operrst <busz.m@outlook.com>
2022-04-12 12:33:49 +0200

    downloading axios to start api

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import MyInput from './components/input/input';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <MyInput/>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/input/input.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+function MyInput() {
+  const [name, setName] = useState("");
+
+  return (
+    <form>
+      <label>
+        Search
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+    </form>
+  );
+}
+// input
+
+export default MyInput;
