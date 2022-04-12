a0e96103aebafe747e477c8c3a96bec078765d83
Operrst <busz.m@outlook.com>
2022-04-12 15:54:19 +0200

    making and table

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 import { useEffect } from "react";
+import DataTable from "./dataTable";
 function MyInput() {
-  const [name, setName] = useState("");
+  const [name, setName] = useState([]);
+
   useEffect(() => {
     axios
       .get(
@@ -22,6 +24,18 @@ function MyInput() {
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
+        <table width="100%" margin="1px" padding="8px" border="1px">
+          <tr>
+            <td>Image</td>
+            <td>Title</td>
+            <td>Wiki</td>
+            <td>Avg price (24h) per slot</td>
+            <td>24h Change</td>
+            <td>7d CHange</td>
+            <td>Sell to trader</td>
+            <td>Insta Profit</td>
+          </tr>
+        </table>
       </label>
     </form>
   );
