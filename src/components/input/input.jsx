7720a2a9110c8836514650307dffac7f3e85caeb
Operrst <busz.m@outlook.com>
2022-04-12 12:56:16 +0200

    geting and tarkov axios. and checking object

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -20,8 +20,13 @@ function MyInput() {
 }
 // input
 
-function DataFetching() {
+function MarketFetching() {
   const [data, setData] = useState([]);
 }
+axios
+  .get("https://tarkov-market.com/api/v1/item?q=btc&x-api-key=QTnLRoCJbP25tEEd")
+  .then((res) => {
+    console.log(res);
+  });
 // ss
 export default MyInput;
